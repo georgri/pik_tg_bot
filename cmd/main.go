c3e9a1738d9573d8f2ddaedd5021bb165cbd0c9d
Georgii Riskov <georgris@gmail.com>
2023-12-09 04:19:24 +0300

    added basic daemon and logging functionality

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"github.com/georgri/pik_tg_bot/pkg/downloader"
-	"github.com/georgri/pik_tg_bot/pkg/telegrambot"
-	"strings"
-)
+import "github.com/georgri/pik_tg_bot/pkg/telegrambot"
 
 func main() {
-	fmt.Printf("Hello world!\n")
-
-	flats, filtered, updateCallback, err := downloader.GetFlats()
-	if err != nil {
-		fmt.Printf("error getting pik url: %v", err)
-	}
-
-	if len(strings.TrimSpace(flats)) == 0 {
-		fmt.Printf("No new flats, aborting; filtered %v", filtered)
-		return
-	}
-
-	fmt.Printf("Got flats: %v", flats)
-
-	err = telegrambot.SendTestMessage(flats)
-	if err != nil {
-		fmt.Printf("error while sending message: %v", err)
-		return
-	}
-
-	err = updateCallback()
-	if err != nil {
-		fmt.Printf("update callback failed: %v", err)
-	}
+	telegrambot.RunForever("testing")
 }
---
+++ pkg/telegrambot/daemon.go
@@ -0,0 +1,70 @@
+package telegrambot
+
+import (
+	"github.com/georgri/pik_tg_bot/pkg/downloader"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+const (
+	invokeEvery = 5 * time.Minute
+
+	logfile = "logs/bot.log"
+)
+
+var EnvType string
+
+func GetEnvType() string {
+	return EnvType
+}
+
+func RunForever(env string) {
+	EnvType = env
+
+	// set up simple logging
+	f, err := os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Printf("error while closing log file: %v", err)
+		}
+	}(f)
+
+	log.SetOutput(f)
+
+	for {
+		RunOnce()
+		time.Sleep(invokeEvery)
+	}
+}
+
+func RunOnce() {
+	flats, filtered, updateCallback, err := downloader.GetFlats()
+	if err != nil {
+		log.Printf("error getting response from pik.ru: %v", err)
+		return
+	}
+
+	if len(strings.TrimSpace(flats)) == 0 {
+		log.Printf("No new flats, aborting; filtered %v", filtered)
+		return
+	}
+
+	log.Printf("Got flats: %v", flats)
+
+	err = SendTestMessage(flats)
+	if err != nil {
+		log.Printf("error while sending message: %v", err)
+		return
+	}
+
+	err = updateCallback()
+	if err != nil {
+		log.Printf("update callback failed: %v", err)
+	}
+}
